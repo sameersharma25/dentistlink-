2d3d1a47031cd0e7c5411c398a367b30b1ab5130
Tushar Agarwal <tushar7250@gmail.com>
2018-05-07 23:16:14 +0530

    missed files

---
+++ src/app/patients/patients.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+import { CurrentUserService } from '../shared/services/current-user.service';
+import { DataSourceService } from '../shared/services/data-source.service';
+import {Email} from '../shared/model/common-model';
+
+@Component({
+  selector: 'app-patients',
+  templateUrl: './patients.component.html',
+  styleUrls: ['./patients.component.scss']
+})
+export class PatientsComponent implements OnInit {
+
+  public patientAction: any = {};
+  public selectedPatient: any = {};
+  public patientList: any = [];
+  public panelOpenState: boolean = true;
+  public patientEditForm: FormGroup;
+
+  constructor(
+    private cus: CurrentUserService,
+    private dss: DataSourceService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.patientAction.label = "";
+    this.patientAction.collapsed = false;
+    this.getAllPatients();
+    this.createForm();
+  }
+  
+  createForm(){
+    this.patientEditForm = this.fb.group({
+      notes: ['']
+    });
+  };
+
+  getAllPatients(){
+    let currentUserMail: Email;
+    let reqObj: any = {};
+    currentUserMail = this.cus.getCurrentUser();
+    reqObj.email = currentUserMail;
+    this.dss.getAllPatientList(reqObj).subscribe(res =>{
+      const response:any = res;
+      if(response.status == 'ok'){
+        this.patientList = response.patients_details;
+        //this.appointmentList = this.getAppointmentList(this.response);
+      }
+    }, err =>{
+      console.log(err);
+    });
+  };
+
+  openPatientAction(data) {
+    if (data) {
+      this.patientAction.label = 'Edit';
+      this.selectedPatient = data;
+      //this.getAppointmentDetails(data);
+      console.log(data);
+      //this.isAppointmentEdit = true;
+    } else {
+      this.patientAction.label = 'Create';
+      //this.isAppointmentEdit = false;
+      //this.createForm();
+    }
+    this.patientAction.collapsed = true;
+  }
+}
---
+++ src/app/shared/model/common-model.ts
@@ -0,0 +1,6 @@
+export class Email{
+    public email: string;
+}
+export class User{
+
+}
