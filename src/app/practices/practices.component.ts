99f75b64985e0dcbaa7ad236e9157888344a48b7
Mbartlett413 <Mbartlett413@me.com>
2018-07-27 16:28:02 -0400

    small adjustments for practice

--- src/app/practices/practices.component.ts
+++ src/app/practices/practices.component.ts
@@ -15,12 +15,12 @@ export class PracticesComponent implements OnInit {
 	isCollapsed1: Boolean = false;
 	isOpen: Boolean = false;
 	hasOtherOptions: boolean = false;
-	options: any = [10,15,20]
+	radiusOp: any[];
+  numLimit = 2;
 	//
 	reqObj: any = {};
-	serviceProvider: any = {};
-	serviceProviderZip: any = {};
-	selectedProvider: any = {};
+	serviceProvider: any = [];
+	selectedProvider: any = [];
 	value = ''; 
 	// Provider Details
   providerFlag: boolean; 
@@ -29,6 +29,7 @@ export class PracticesComponent implements OnInit {
 	providerName: string;
 	providerZipCode: string;
 	providerContact: string;
+  providerDistance: number;
 
 	providerAddress: string;
 	providerCity: string;
@@ -61,7 +62,8 @@ export class PracticesComponent implements OnInit {
   constructor(
   	private cus: CurrentUserService,
     private dss: DataSourceService,
-    private fb: FormBuilder,) { }
+    private fb: FormBuilder,) {
+    this.radiusOp=[5,10,15]; }
 
   ngOnInit() {
   	this.getProviders();
@@ -85,7 +87,6 @@ export class PracticesComponent implements OnInit {
   	this.reqObj.email = this.cus.getCurrentUser();
   	this.dss.allProviders(this.reqObj).subscribe(res => {
   		this.serviceProvider = res
-  		this.serviceProviderZip = res
   		console.log("All Providers", res);
   	})
   } //End getProviders
@@ -106,36 +107,31 @@ export class PracticesComponent implements OnInit {
   }
 
  openProviderAction(data){
- 	this.selectedProvider = data
- 	//Basic Info
- 	this.providerName = data.Name
-   this.providerFlag = data.Flagged_Provider__c
-   this.providerDescription = data.Description
-   this.providerDescription2 = data.External_Description__c
- 	this.providerZipCode = data.BillingAddress.postalCode
- 	this.providerState = data.BillingAddress.state
- 	this.providerAddress = data.BillingAddress.street
- 	this.providerCity = data.BillingAddress.city
- 	this.providerPhone = data.Phone
- 	this.providerEmail = data.Provider_Email__c
-  	this.isCollapsed1 = true;
-  	//checkbox
-  	this.providerApple = data.Apple_Health__c
-  	this.providerDelta = data.Delta_Dental_of_WA__c
-  	this.providerPediatric = data.Pediatric__c
-  	this.providerAdult = data.Adult__c
-  	//missing cleaning,pain,extraction,denture
-  	this.providerOrtho = data.Orthodontics__c
-  	this.providerCleaning = data.Cleaning__c
-	this.providerPain = data.Pain__c
-	this.providerExtraction = data.Extractions__c
-	this.providerOrtho = data.Orthodontics__c
-	this.providerDentures = data.Dentures__c
-  	//Long & Lat
-  	this.lat = data.Geolocation__c.latitude
-  	console.log("Lat",this.lat)
-  	this.lng = data.Geolocation__c.longitude
-  	console.log("lng",this.lng)
+      //Basic Info
+      this.providerName = data.Name
+      this.providerFlag = data.Flagged_Provider__c
+      this.providerDescription = data.Description
+      this.providerDescription2 = data.External_Description__c
+      this.providerZipCode = data.BillingAddress.postalCode
+      this.providerState = data.BillingAddress.state
+      this.providerAddress = data.BillingAddress.street
+      this.providerCity = data.BillingAddress.city
+      this.providerPhone = data.Phone
+      this.providerEmail = data.Provider_Email__c
+      this.isCollapsed1 = true;
+      //checkbox
+      this.providerApple = data.Apple_Health__c
+      this.providerDelta = data.Delta_Dental_of_WA__c
+      this.providerPediatric = data.Pediatric__c
+      this.providerAdult = data.Adult__c
+      //missing cleaning,pain,extraction,denture
+      this.providerOrtho = data.Orthodontics__c
+      this.providerCleaning = data.Cleaning__c
+      this.providerPain = data.Pain__c
+      this.providerExtraction = data.Extractions__c
+      this.providerOrtho = data.Orthodontics__c
+      this.providerDentures = data.Dentures__c
+
 
  }
 
